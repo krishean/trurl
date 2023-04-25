f4582854d4a93f4438d4e8c5259b1ecbaf90190d
Krishean Draconis <70195763+krishean@users.noreply.github.com>
2023-04-24 22:18:03 -0400

    Change test.sh back to using ctest

--- winbuild/test.sh
+++ winbuild/test.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 GITHUB_WORKSPACE=$(dirname "$(realpath "$0")")
 cd "$GITHUB_WORKSPACE"
-if [ -z "$PYTHON3" ];then PYTHON3="python3";fi
+#if [ -z "$PYTHON3" ];then PYTHON3="python3";fi
 presets=("x64-debug" "x64-release" "x86-debug" "x86-release")
 for preset in "${presets[@]}";do
-    if [ -d "$GITHUB_WORKSPACE/out/install/${preset}/bin" ];then
-        cd "$GITHUB_WORKSPACE/out/install/${preset}/bin"
+    #if [ -d "$GITHUB_WORKSPACE/out/install/${preset}/bin" ];then
+    #    cd "$GITHUB_WORKSPACE/out/install/${preset}/bin"
+    if [ -d "$GITHUB_WORKSPACE/out/build/${preset}" ];then
+        cd "$GITHUB_WORKSPACE/out/build/${preset}"
         echo "Testing ${preset}..."
         #perl "$GITHUB_WORKSPACE/../test.pl"
-        $PYTHON3 "$GITHUB_WORKSPACE/../test.py"
-        #ctest -V
+        #$PYTHON3 "$GITHUB_WORKSPACE/../test.py"
+        ctest -V
     else
         echo " note: The ${preset} directory does not exist."
     fi
