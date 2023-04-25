07f7cdee18cedbee97f465f3fa40d659c21d3e59
Krishean Draconis <70195763+krishean@users.noreply.github.com>
2023-04-24 22:02:36 -0400

    Add variable for python3 to test.sh

--- winbuild/test.sh
+++ winbuild/test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 GITHUB_WORKSPACE=$(dirname "$(realpath "$0")")
 cd "$GITHUB_WORKSPACE"
+if [ -z "$PYTHON3" ];then PYTHON3="python3";fi
 presets=("x64-debug" "x64-release" "x86-debug" "x86-release")
 for preset in "${presets[@]}";do
     if [ -d "$GITHUB_WORKSPACE/out/install/${preset}/bin" ];then
         cd "$GITHUB_WORKSPACE/out/install/${preset}/bin"
         echo "Testing ${preset}..."
         #perl "$GITHUB_WORKSPACE/../test.pl"
-        python3 "$GITHUB_WORKSPACE/../test.py"
+        $PYTHON3 "$GITHUB_WORKSPACE/../test.py"
         #ctest -V
     else
         echo " note: The ${preset} directory does not exist."
